5fcd90a825ddaba0d8ef285c97d872f2e609acc0
Raju Kumar <rakumar@ptc.com>
2017-10-07 17:11:49 +0530

    changed build steps

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 /**
  * Created by rakumar on 07-10-2017.
  */
+const express = require('express');
 const path = require('path');
+const app = express();
 // ...
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
