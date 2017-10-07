df3e9ebdcc3c43e292a9bdde759b5a9471cec717
Raju Kumar <rakumar@ptc.com>
2017-10-07 16:15:59 +0530

    first deploy

---
+++ server.js
@@ -0,0 +1,10 @@
+/**
+ * Created by rakumar on 07-10-2017.
+ */
+const path = require('path');
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
